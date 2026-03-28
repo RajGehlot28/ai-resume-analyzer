a80034ac6ba5c7ed0ccedfed21002a0a18072a6b
RajGehlot28 <rajgehlot2007@gmail.com>
2026-03-28 09:51:55 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -14,6 +14,7 @@ connectDB();
 app.use("/api/auth", authRoutes);
 app.use("/api/resume", resumeRoutes);
 
-app.listen(process.env.PORT, () => {
-    console.log("Server running on port process.env.PORT");
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server is running on port ${PORT}`);
 });
